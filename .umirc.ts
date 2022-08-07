2c3ba1b329078f9a0b7d937f2e90347de7c79d96
shawn <30490036+captain1023@users.noreply.github.com>
2022-08-07 18:32:39 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'Shawn Blog',
+  title: 'Blog',
   favicon:
     'https://raw.githubusercontent.com/captain1023/picGo/master/img/20220807182212.png',
-  logo: 'https://raw.githubusercontent.com/captain1023/picGo/master/img/20220807182212.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
   base: './',
